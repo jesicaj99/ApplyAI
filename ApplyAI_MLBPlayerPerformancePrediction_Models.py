3b9012570c0bb5aa495da5a9147308dce2675863
Siddharth Nair <nair.sid01@gmail.com>
2021-10-24 09:49:25 -0500

    Update ApplyAI_MLBPlayerPerformancePrediction_Models.py

--- ApplyAI_MLBPlayerPerformancePrediction_Models.py
+++ ApplyAI_MLBPlayerPerformancePrediction_Models.py
@@ -7,6 +7,17 @@ from scikit.linear_model import ElasticNet
 from sklearn.neighbors import KNeighborsClassifier
 
 def performancelinear():
+    #first attempt approach with hitter data/stats, then depending on efficacy tweak & add defensive stats
+    # after hitter stats attempt, run again w/ pitcher data
+    # as we don't have framing data currently it might be better to include catchers as just typical positional players
+    hitterdataset = pd.read_csv('')
+    x = dataset.iloc[:,:-1].values   #independent variable array 1st factor
+    y = dataset.iloc[:,1].values  #dependent variable vector 2nd factor
+    # add additional factors based off of rows in the relevant cavs, (player.csv for players, pitcher.csv for pitchers)
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1/3,random_state=0)
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)
+    y_pred = regressor.predict(x_test)
 
 def performancelasso():
 
