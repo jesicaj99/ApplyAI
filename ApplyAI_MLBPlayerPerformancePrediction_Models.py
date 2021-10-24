c395040ea0b8cc39365ec5a8f793247d1c41a11c
Siddharth Nair <nair.sid01@gmail.com>
2021-10-23 23:20:17 -0500

    Create ApplyAI_MLBPlayerPerformancePrediction_Models.py
    Basis structure of models in place, theoretically can be an aio program if the dataset is properly cleaned and read into the python file.

---
+++ ApplyAI_MLBPlayerPerformancePrediction_Models.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from scikit.linear_model import LinearRegression
+from scikit.linear_model import Lasso
+from scikit.linear_model import ElasticNet
+from sklearn.neighbors import KNeighborsClassifier
+
+def performancelinear():
+
+def performancelasso():
+
+def performanceelasticnet():
+
+def performanceKNNvisualization():
+
+def main():
+
+if __name__ == "__main__":
+    main()
+
