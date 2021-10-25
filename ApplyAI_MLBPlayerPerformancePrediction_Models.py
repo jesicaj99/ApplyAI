2207f00a2b37934d86b5f962a6b09dd70fa1557a
Siddharth Nair <nair.sid01@gmail.com>
2021-10-25 18:36:43 -0500

    Notes on variables being used for predictions

--- ApplyAI_MLBPlayerPerformancePrediction_Models.py
+++ ApplyAI_MLBPlayerPerformancePrediction_Models.py
@@ -10,11 +10,14 @@ from sklearn.metrics import mean_absolute_error,r2_score
 
 def performancelinear():
     #first attempt approach with hitter data/stats, then depending on efficacy tweak & add defensive stats
+    # NOTE -> hitting stats include (walks, pitches taken, average, obp+slug)
+    # DOUBLE NOTE-> defensive stats will have to only include fielding (outcomes of fielding are (double play, assist, error, passed ball *catcher specific might disregard for now*)
+    # TRIPLE NOTE-> add baserunning as a variable as well(outcomes of baserunning are stealing, caught stealing, picked off)
     # after hitter stats attempt, run again w/ pitcher data
     # as we don't have framing data currently it might be better to include catchers as just typical positional players
     hitterdataset = pd.read_csv('')
-    x = dataset.iloc[:,:-1].values   #independent variable array 1st factor
-    y = dataset.iloc[:,1].values  #dependent variable vector 2nd factor
+    x = hitterdataset.iloc[:,:-1].values   #independent variable array 1st factor
+    y = hitterdataset.iloc[:,1].values  #dependent variable vector 2nd factor
     # add additional factors based off of rows in the relevant cavs, (player.csv for players, pitcher.csv for pitchers)
     x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1/3,random_state=0)
     regressor = LinearRegression()
@@ -36,9 +39,14 @@ def performanceelasticnet():
     y_pred = regressor.predict(x_test)
     return (r2_score(y_test,y_pred))
 
-def performanceKNNvisualization():
+#def performanceKNNvisualization():
 
 def main():
+    #playerperformancedata 
+    performancelinear()
+    performancelasso()
+    performanceelasticnet()
+    #performanceKNNvisualization()
 
 if __name__ == "__main__":
     main()
