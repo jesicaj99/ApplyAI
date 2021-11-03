54853d9439ba708b36e6bfabd7ccd7b6a02c5b87
Siddharth Nair <nair.sid01@gmail.com>
2021-11-03 15:03:23 -0500

    Update DataParser.py
    Tabs vs. Spaces fixed

--- DataParser.py
+++ DataParser.py
@@ -15,91 +15,93 @@ war_df = pd.read_csv('FanGraphs Leaderboard.csv')
 
 #gets rid of unnecessary columns in hitter csv
 def clean_sorted_hitter():
-    hitter_by_game_df.drop(['H-AB', 'AB', 'H', '#P', 'Game', 'Team', 'Hitter Id'], axis = 1)
-    sorted_hitter_df = hitter_by_game_df.sort_values(by='Hitters')
-    return sorted_hitter_df
+	hitter_by_game_df.drop(['H-AB', 'AB', 'H', '#P', 'Game', 'Team', 'Hitter Id'], axis = 1)
+	sorted_hitter_df = hitter_by_game_df.sort_values(by='Hitters')
+	return sorted_hitter_df
 
 #gets rids of unnecessary columns in pitcher csv
 def clean_sorted_pitcher():
-    pitcher_by_game_df.drop(['R', 'ER', 'PC', 'Game', 'Team', 'Extra', 'Pitcher Id'], axis = 1)
-    sorted_pitcher_df = pitcher_by_game_df.sort_values(by='Pitchers')
-    return sorted_pitcher_df
+	pitcher_by_game_df.drop(['R', 'ER', 'PC', 'Game', 'Team', 'Extra', 'Pitcher Id'], axis = 1)
+	sorted_pitcher_df = pitcher_by_game_df.sort_values(by='Pitchers')
+	return sorted_pitcher_df
 
 #gets rids of unnecessary fielding columns
 def clean_sorted_fielding():
-    del fielding_by_game_df['Game']
-    sorted_fielding_df = fielding_by_game_df.sort_values(by='Team')
-    return sorted_fielding_df
+	del fielding_by_game_df['Game']
+	sorted_fielding_df = fielding_by_game_df.sort_values(by='Team')
+	return sorted_fielding_df
 
 #gets rids of unnececessary baserunning columns
 def clean_sorted_baserunning():
-    del baserunning_by_game_df['Game']
-    sorted_baserunning_df = baserunning_by_game_df.sort_values(by='Team')
-    return sorted_baserunning_df
+	del baserunning_by_game_df['Game']
+	sorted_baserunning_df = baserunning_by_game_df.sort_values(by='Team')
+	return sorted_baserunning_df
 
 def clean_warp_hitter():
-    warp_hitter_df.drop(['bpid', 'mlbid', 'Age', 'DRC+', '+/-', 'PA', 'R', 'RBI','ISO', 'K', 'BB%','Whiff%'], axis = 1)
-    sorted_warphitter_df = warp_hitter_df.sort_values(by='WARP')
-    return sorted_warphitter_df
+	warp_hitter_df.drop(['bpid', 'mlbid', 'Age', 'DRC+', '+/-', 'PA', 'R', 'RBI','ISO', 'K', 'BB%','Whiff%'], axis = 1)
+	sorted_warphitter_df = warp_hitter_df.sort_values(by='WARP')
+	return sorted_warphitter_df
 
 def clean_warp_pitcher():
-    warp_pitcher_df.drop(['bpid', 'mlbid', 'DRA-', 'DRA+', 'DRA SD', 'cFIP', 'GS', 'W','L', 'ERA', 'RA9','Whiff%'], axis = 1)
-    sorted_warppitcher_df = warp_pitcher_df.sort_values(by='WARP')
-    return sorted_warppitcher_df
+	warp_pitcher_df.drop(['bpid', 'mlbid', 'DRA-', 'DRA+', 'DRA SD', 'cFIP', 'GS', 'W','L', 'ERA', 'RA9','Whiff%'], axis = 1)
+	sorted_warppitcher_df = warp_pitcher_df.sort_values(by='WARP')
+	return sorted_warppitcher_df
 
 def clean_war():
-    war_df.drop(['playerid','Team','Pos'])
-    sorted_war_df = war_df.sort_values(by='Total WAR')
-    return sorted_war_df
+	war_df.drop(['playerid','Team','Pos'])
+	sorted_war_df = war_df.sort_values(by='Total WAR')
+	return sorted_war_df
 
 def clean_defensive_players():
-    oaa_hitter_df.drop(['player_id','display_team_name','year','primary_pos_formatted'],axis = 1)
-    fielding_df.drop(['Game','Team'])
-    sorted_fielding_df = fielding_df.sort_values(by='Stat')
-    
-    playerscurrent = []
-    playerstotal = []
-    scores = []
-    #TODO, assigning values for various defensive plays to the players that participated in them, keep track of them via secondary array (hashmap w/ key and value?)
-    for key,value in sorted_fielding_df.iteritems():
-        statlines = value.split(',')
-        if statlines[0] == 'DP':
-            for x in int(statlines[1][:1]):
-                playerscurrent = re.findall(capitalized_words, statlines[x+2]))
-            for x in range(0,len(playerscurrent)):
-                for y in range(0,len(playerstotal)):
-                    if playerscurrent[x] == playerstotal[y]:
-                        scores[x]+=1
-                    else:
-                        playerstotal.append(playerscurrent[x])
-                        scores.append(1)
-        elif statlines[0] == 'Assists':
-            for x in int(statlines[1][:1]):
-                playerscurrent = re.findall(capitalized_words, statlines[x+2]))
-            for x in range(0,len(playerscurrent)):
-                for y in range(0,len(playerstotal)):
-                    if playerscurrent[x] == playerstotal[y]:
-                        scores[x]+=.5
-                    else:
-                        playerstotal.append(playerscurrent[x])
-                        scores.append(.5)
-        elif statlines[0] == 'E':
-            for x in int(statlines[1][:1]):
-                playerscurrent = re.findall(capitalized_words, statlines[x+2]))
-            for x in range(0,len(playerscurrent)):
-                for y in range(0,len(playerstotal)):
-                    if playerscurrent[x] == playerstotal[y]:
-                        scores[x]-=.5
-                    else:
-                        playerstotal.append(playerscurrent[x])
-                        scores.append(-.5) 
-        
-    defensivevalues_hash_table = HashTable(len(scores))
-    for x in range(0,len(scores)):
-        defensivevalues_hash_table.set_val(playerstotal[x], scores[x])
-    return defensivevalues_hash_table
+	oaa_hitter_df.drop(['player_id','display_team_name','year','primary_pos_formatted'],axis = 1)
+	fielding_df.drop(['Game','Team'])
+	sorted_fielding_df = fielding_df.sort_values(by='Stat')
 
+	scores = []
+	playerscurrent = []
+	playerstotal = []
+	outssaved = []
 
+	#TODO, assigning values for various defensive plays to the players that participated in them, keep track of them via secondary array (hashmap w/ key and value?)
+	for key,value in sorted_fielding_df.iteritems():
+		statlines = value.split(',')
+		if statlines[0] == 'DP':
+			for x in int(statlines[1][:1]):
+				playerscurrent = re.findall(capitalized_words, statlines[x+2]))
+			for x in range(0,len(playerscurrent)):
+				for y in range(0,len(playerstotal)):
+					if playerscurrent[x] == playerstotal[y]:
+						outssaved[x]+=1
+					else:
+						playerstotal.append(playerscurrent[x])
+						outssaved.append(1)
+		elif statlines[0] == 'Assists':
+			for x in int(statlines[1][:1]):
+				playerscurrent = re.findall(capitalized_words, statlines[x+2]))
+			for x in range(0,len(playerscurrent)):
+				for y in range(0,len(playerstotal)):
+					if playerscurrent[x] == playerstotal[y]:
+						outssaved[x]+=.5
+					else:
+						playerstotal.append(playerscurrent[x])
+						outssaved.append(.5)
+		elif statlines[0] == 'E':
+			for x in int(statlines[1][:1]):
+				playerscurrent = re.findall(capitalized_words, statlines[x+2]))
+			for x in range(0,len(playerscurrent)):
+				for y in range(0,len(playerstotal)):
+					if playerscurrent[x] == playerstotal[y]:
+						outssaved[x]-=1
+					else:
+						playerstotal.append(playerscurrent[x])
+						outssaved.append(-1) 
+	for x in range(0, len(outssaved)):
+		scores[x] = (outssaved[x] / 3)
+
+	defensivevalues_hash_table = HashTable(len(scores))
+	for x in range(0,len(scores)):
+		defensivevalues_hash_table.set_val(playerstotal[x], scores[x])
+	return defensivevalues_hash_table
 
 class HashTable:
 
