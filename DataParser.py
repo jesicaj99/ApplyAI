7a78367c01307f04ec1ae47eb37e6bdacaf9b8de
Siddharth Nair <nair.sid01@gmail.com>
2021-11-03 12:35:40 -0500

    Hashtable implementation & data parsing of defensive stats
    Finished first iteration of parsing defensive stats for the plays and assigning values to them for the players involved. Created a hashmap to map the players names to the values (player name key defensive values created value)

--- DataParser.py
+++ DataParser.py
@@ -47,18 +47,151 @@ def clean_warp_pitcher():
     sorted_warppitcher_df = warp_pitcher_df.sort_values(by='WARP')
     return sorted_warppitcher_df
 
-def clean_defensive():
+def clean_defensive_players():
     oaa_hitter_df.drop(['player_id','display_team_name','year','primary_pos_formatted'],axis = 1)
     fielding_df.drop(['Game','Team'])
     sorted_fielding_df = fielding_df.sort_values(by='Stat')
-    players = []
     
+    playerscurrent = []
+    playerstotal = []
+    scores = []
     #TODO, assigning values for various defensive plays to the players that participated in them, keep track of them via secondary array (hashmap w/ key and value?)
     for key,value in sorted_fielding_df.iteritems():
         statlines = value.split(',')
         if statlines[0] == 'DP':
             for x in int(statlines[1][:1]):
-                players = re.findall(capitalized_words, statlines[x+2]))
-            for x in players:
-                
+                playerscurrent = re.findall(capitalized_words, statlines[x+2]))
+            for x in range(0,len(playerscurrent)):
+                for y in range(0,len(playerstotal)):
+                    if playerscurrent[x] == playerstotal[y]:
+                        scores[x]+=1
+                    else:
+                        playerstotal.append(playerscurrent[x])
+                        scores.append(1)
+        elif statlines[0] == 'Assists':
+            for x in int(statlines[1][:1]):
+                playerscurrent = re.findall(capitalized_words, statlines[x+2]))
+            for x in range(0,len(playerscurrent)):
+                for y in range(0,len(playerstotal)):
+                    if playerscurrent[x] == playerstotal[y]:
+                        scores[x]+=.5
+                    else:
+                        playerstotal.append(playerscurrent[x])
+                        scores.append(.5)
+        elif statlines[0] == 'E':
+            for x in int(statlines[1][:1]):
+                playerscurrent = re.findall(capitalized_words, statlines[x+2]))
+            for x in range(0,len(playerscurrent)):
+                for y in range(0,len(playerstotal)):
+                    if playerscurrent[x] == playerstotal[y]:
+                        scores[x]-=.5
+                    else:
+                        playerstotal.append(playerscurrent[x])
+                        scores.append(-.5) 
+        
+    hash_table = HashTable(len(scores))
+    for x in range(0,len(scores)):
+        hash_table.set_val(playerstotal[x], scores[x])
+
+
+
+# search/access a record with key
+print(hash_table.get_val('portal@example.com'))
+
+class HashTable:
+
+	# Create empty bucket list of given size
+	def __init__(self, size):
+		self.size = size
+		self.hash_table = self.create_buckets()
+
+	def create_buckets(self):
+		return [[] for _ in range(self.size)]
+
+	# Insert values into hash map
+	def set_val(self, key, val):
+		
+		# Get the index from the key
+		# using hash function
+		hashed_key = hash(key) % self.size
+		
+		# Get the bucket corresponding to index
+		bucket = self.hash_table[hashed_key]
+
+		found_key = False
+		for index, record in enumerate(bucket):
+			record_key, record_val = record
+			
+			# check if the bucket has same key as
+			# the key to be inserted
+			if record_key == key:
+				found_key = True
+				break
+
+		# If the bucket has same key as the key to be inserted,
+		# Update the key value
+		# Otherwise append the new key-value pair to the bucket
+		if found_key:
+			bucket[index] = (key, val)
+		else:
+			bucket.append((key, val))
+
+	# Return searched value with specific key
+	def get_val(self, key):
+		
+		# Get the index from the key using
+		# hash function
+		hashed_key = hash(key) % self.size
+		
+		# Get the bucket corresponding to index
+		bucket = self.hash_table[hashed_key]
+
+		found_key = False
+		for index, record in enumerate(bucket):
+			record_key, record_val = record
+			
+			# check if the bucket has same key as
+			# the key being searched
+			if record_key == key:
+				found_key = True
+				break
+
+		# If the bucket has same key as the key being searched,
+		# Return the value found
+		# Otherwise indicate there was no record found
+		if found_key:
+			return record_val
+		else:
+			return "No record found"
+
+	# Remove a value with specific key
+	def delete_val(self, key):
+		
+		# Get the index from the key using
+		# hash function
+		hashed_key = hash(key) % self.size
+		
+		# Get the bucket corresponding to index
+		bucket = self.hash_table[hashed_key]
+
+		found_key = False
+		for index, record in enumerate(bucket):
+			record_key, record_val = record
+			
+			# check if the bucket has same key as
+			# the key to be deleted
+			if record_key == key:
+				found_key = True
+				break
+		if found_key:
+			bucket.pop(index)
+		return
+
+	# To print the items of hash map
+	def __str__(self):
+		return "".join(str(item) for item in self.hash_table)
+
+
+
+
                 
